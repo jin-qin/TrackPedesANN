3761a10f70a3649109db19992e554e26ed764cf1
Tim Hartz <git@timhartz.de>
2016-05-31 18:01:47 -0700

    implement correct target probability maps

--- src/caltech_loader.py
+++ src/caltech_loader.py
@@ -349,21 +349,8 @@ class CaltechLoader:
         self.loadDataSet(False)
 
         # TODO get test data
-        XPrevious, XCurrent = self.testSamplesPrevious, self.testSamplesCurrent
-        Yall = self.calcTargetProbMap(XCurrent)
-        return XPrevious, XCurrent , Yall
-
-    # source: http://stackoverflow.com/questions/29731726/how-to-calculate-a-gaussian-kernel-matrix-efficiently-in-numpy
-    def gkern(self, kernlen=21, nsig=3):
-        """Returns a 2D Gaussian kernel array."""
-
-        interval = (2 * nsig + 1.) / (kernlen)
-        x = np.linspace(-nsig - interval / 2., nsig + interval / 2., kernlen + 1)
-        kern1d = np.diff(st.norm.cdf(x))
-        kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-        kernel = kernel_raw / kernel_raw.sum()
-        return kernel
-
+        XPrevious, XCurrent,Y = self.testSamplesPrevious, self.testSamplesCurrent, self.testY
+        return XPrevious, XCurrent , Y
 
     def softmax(self, allScores):
 
